ffe98fff23e5817e5b5667aa664f8c4f4644f97f
Ace <omarace95@gmail.com>
2022-02-19 20:18:46 -0800

    unideal solution but works

--- src/components/about/aboutTeamCard.js
+++ src/components/about/aboutTeamCard.js
@@ -6,8 +6,8 @@ const AboutTeamCard = ({ imgSrc, position, web }) => {
     <Fade bottom cascade>
       <div className="w-full h-96 relative overflow-hidden rounded-xl flex flex-col justify-end items-center">
         <img className="object-cover h-full w-full absolute" src={imgSrc}></img>
-        <h1 className="bg-white absolute p-2 rounded-xl w-3/4 mb-4 text-center">
-        {position}
+        <h1 className="bg-lightblack text-white absolute p-2 rounded-xl w-3/4 mb-4 text-center">
+        <a href={web}>{position}</a>
         </h1>
       </div>
     </Fade>
